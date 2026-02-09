cc9a9f4582cb2adaa3c8f14717d5c4ffd4ba635f
Shivam <sshivam.mpi0123@gmail.com>
2026-02-09 16:43:14 +0530

    [Shivam] Add. completed setup of sonarcube

---
+++ collections-practice/gcr-codebase/sonarcube/sonarcube/src/main/java/Sonarcube/sonarcube/App.java
@@ -0,0 +1,13 @@
+package Sonarcube.sonarcube;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
